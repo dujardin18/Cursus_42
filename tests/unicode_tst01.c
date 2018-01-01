d714855c85bb2421d936751484977ef2b8078c44
Felix HERBINET <fherbine@e1r3p2.42.fr>
2018-01-01 19:28:08 +0100

    tst update 01/01

--- tests/unicode_tst01.c
+++ tests/unicode_tst01.c
@@ -1,13 +1,15 @@
 #include <locale.h>
 #include <stdio.h>
 #include <wchar.h>
+#include <unistd.h>
 
 int n_oct(wchar_t c)
 {
 	int t;
 	int len = 1;
 	t = 8;
-	while (c >> t!= 0)
+	char ts;
+	while (c >> t!= 0x0)
 	{
 		t += 8;
 		len++;
@@ -15,15 +17,20 @@ int n_oct(wchar_t c)
 	return (len);
 }
 
-int ft_wstrlen(wchar_t *wstr)
+void ft_wputc(wint_t c)
 {
+	int t;
+	int car;
+	int len;
+	len = n_oct(c);
 }
 
 int main(void)
 {
 	setlocale(LC_ALL, "");
-	wchar_t c = L'☾';
+	wchar_t c = L'ሴ';
 	printf("\n%x\n", c);
-	printf("%d", lenoct(c));
+	printf("%d", n_oct(c));
+//	ft_wputc(c);
 	return (0);
 }
