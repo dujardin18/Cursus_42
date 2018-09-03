d54b07a43ba722182542e129fcfe94ce91125fc4
fherbine <felix.herbinet@yahoo.com>
2018-09-03 08:05:47 +0200

    Adding defines for standard IOs

--- libft/defines/references.h
+++ libft/defines/references.h
@@ -6,7 +6,7 @@
 /*   By: fherbine <fherbine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/03 04:32:16 by fherbine          #+#    #+#             */
-/*   Updated: 2018/09/03 04:34:34 by fherbine         ###   ########.fr       */
+/*   Updated: 2018/09/03 08:05:06 by fherbine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,9 @@
 # define U8_MAX 0xff
 # define U16_MAX 0xffff
 # define U32_MAX 0xffffffff
+
+# define STDIN 0
+# define STDOUT 1
+# define STDERR 2
+
 #endif
